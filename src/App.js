3b19b042be9109ce987e77c0a06df613674c4893
Celig <github@cela.anonaddy.com>
2021-11-12 16:14:29 +0100

    Fix invalid DOM property warning

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
       <div className="outputContainer">
         <h1 className="title">Calculate your Body Index Mass</h1>
         <div className="body">
-          <p class="text">Your BMI is <b>24.5</b>, indicating your weight is in the <b>Normal</b> category for adults of you height.</p>
-          <p class="text">For your height, a normal weight range would be from <b>56.7</b> to <b>76.3</b> kilograms.</p>
-          <p class="text">Maintaining a healthy weight may reduce the risk of chronic diseases associated with overweight and obesity.</p>
+          <p className="text">Your BMI is <b>24.5</b>, indicating your weight is in the <b>Normal</b> category for adults of you height.</p>
+          <p className="text">For your height, a normal weight range would be from <b>56.7</b> to <b>76.3</b> kilograms.</p>
+          <p className="text">Maintaining a healthy weight may reduce the risk of chronic diseases associated with overweight and obesity.</p>
         </div>
       </div>
 
