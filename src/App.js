0d46e0c87f4604051be362186b4b548d398a8733
Celig <github@cela.anonaddy.com>
2021-11-12 20:35:44 +0100

    Add function to get bmi category for the chart bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.sass';
 function App() {
   const [height, setHeight] = useState(175)
   const [weight, setWeight] = useState(75)
-  const [bmi, setBMI] = useState('')
+  const [bmi, setBMI] = useState(24.49)
+  const [bmiCategory, setBmiCategory] = useState()
+  const [chart, setChart] = useState()
 
   useEffect(() => {
     calculateBMI()
-  }, [height, weight])
+    BMIcategory()
+  }, [bmi, weight, weight, bmiCategory, chart])
 
   const handleHeightInput = (e) => {
     setHeight(e.target.value)
@@ -22,13 +25,41 @@ function App() {
     setBMI(weight / (height / 100) ** 2)
   }
 
+  const BMIcategory = () => {
+    if (bmi < 15) {
+      setBmiCategory('Very severly underweight')
+      setChart('12.5%')
+    } else if (bmi >= 15 && bmi < 16 ) {
+      setBmiCategory('Severly underweight')
+      setChart('25%')
+    } else if (bmi >= 16 && bmi < 18.5) {
+      setBmiCategory('Underweight')
+      setChart('37.5%')
+    } else if (bmi >= 18.5 && bmi < 25) {
+      setBmiCategory('Normal')
+      setChart('50%')
+    } else if (bmi >= 25 && bmi < 30) {
+      setBmiCategory('Overweight')
+      setChart('62.5%')
+    } else if (bmi >= 30 && bmi < 35) {
+      setBmiCategory('Moderately obese')
+      setChart('75%')
+    } else if (bmi >= 35 && bmi < 40) {
+      setBmiCategory('Severely obese')
+      setChart('87.5%')
+    } else if (bmi > 40) {
+      setBmiCategory('Very severly obese')
+      setChart('100%')
+    }
+  }
+
   return (
     <div className="app">
 
       <div className="outputContainer">
         <h1 className="title">Calculate your Body Index Mass</h1>
         <div className="body">
-          <p className="text">Your BMI is <b>{bmi.toFixed(2)}</b>, indicating your weight is in the <b>Normal</b> category for adults of you height.</p>
+          <p className="text">Your BMI is <b>{bmi.toFixed(2)}</b>, indicating your weight is in the <b>{bmiCategory}</b> category for adults of you height.</p>
           <p className="text">For your height, a normal weight range would be from <b>56.7</b> to <b>76.3</b> kilograms.</p>
           <p className="text">Maintaining a healthy weight may reduce the risk of chronic diseases associated with overweight and obesity.</p>
         </div>
@@ -51,7 +82,7 @@ function App() {
           <h3 className="bmi">{bmi.toFixed(2)}</h3>
         </div>
         <div className="chart">
-          <div className="arrow"></div>
+          <div className="arrow" style={{ margin: `0 ${chart}` }}></div>
         </div>
       </div>
 
