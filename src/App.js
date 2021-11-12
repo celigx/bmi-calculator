b6dd711d166d5ee794b38c03f8470595dd6cea33
Celig <github@cela.anonaddy.com>
2021-11-12 15:40:04 +0100

    Add input card data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,31 @@ function App() {
         </div>
       </div>
 
+      <div className="inputContainer">
+        <p className="title">Find out your <b>BMI</b></p>
+        <div className="buttonContainer">
+          <a>KG</a>
+          <a>LBS</a>
+        </div>
+        <div className="heightWeightContainer">
+          <div className="height">
+            <p className="text">Height (cm)</p>
+            <input type="number" placeholder="175" />
+          </div>
+          <div className="weight">
+            <p className="text">Weight (kg)</p>
+            <input type="number" placeholder="75" />
+          </div>
+        </div>
+        <div className="bmiContainer">
+          <p className="text">Your Body Mass Index (BMI)</p>
+          <h3 className="bmi">24.5</h3>
+        </div>
+        <div className="chart">
+          <div className="arrow"></div>
+        </div>
+      </div>
+
     </div>
   );
 }
